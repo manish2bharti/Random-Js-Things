7b868bd2a7537a6c0e9e001d1006a76011749f82
Manish Bharti <manish2bharti@users.noreply.github.com>
2021-04-12 00:51:59 +0530

    Update prototype-inheritance.js

--- prototype-inheritance.js
+++ prototype-inheritance.js
@@ -1,3 +1,10 @@
+// Basically everyobject has property called prototype where you can add methods and properties to it. 
+// And when you create other objects from this object , the newly created object will automatically inherit the prperty of the parent. 
+// But not by including in its own property but instead it uses from its parent prototype properties and methos. 
+// And the way it work is when u call a particular method or object it frst looks at its own properties to see if its there 
+// if its not there then it will look at its parent.and if its find it there then it will execute it.
+
+
 var Job = function(){
 	this.pays = true;
 }
@@ -21,3 +28,15 @@ var softwarePosition2 = new TechJob('AngularJS Programmer', false);
 
 console.log(softwarePosition.print())
 console.log(softwarePosition2.print())
+
+//--------------------------------------------------------
+// let car = function(model){
+// this.model = model;
+// }
+
+// car.prototype.getModel = function(){
+// return this.model;
+// }
+
+// let toyota = new car('toyota')
+// console.log(toyota.getModel());
